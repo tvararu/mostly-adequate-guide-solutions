243ae1b582b083f6685ff748c55838b221ad433b
Theodor Vararu <theo@vararu.org>
2015-12-28 21:36:53 +0000

    Start compose exercises.

--- compose.js
+++ compose.js
@@ -1,1 +1,4 @@
-var R = require('ramda')
+const R = require('ramda')
+
+// use _.compose() to rewrite the function below. Hint: _.prop() is curried.
+exports.isLastInStock = R.compose(R.prop('in_stock'), R.last)
---
+++ compose.spec.js
@@ -0,0 +1,20 @@
+const test = require('tape')
+const E = require('./compose')
+
+const CARS = [
+  {name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true},
+  {name: 'Spyker C12 Zagato', horsepower: 650, dollar_value: 648000, in_stock: false},
+  {name: 'Jaguar XKR-S', horsepower: 550, dollar_value: 132000, in_stock: false},
+  {name: 'Audi R8', horsepower: 525, dollar_value: 114200, in_stock: false},
+  {name: 'Aston Martin One-77', horsepower: 750, dollar_value: 1850000, in_stock: true},
+  {name: 'Pagani Huayra', horsepower: 700, dollar_value: 1300000, in_stock: false}
+]
+
+test('isLastInStock', (assert) => {
+  var input = CARS
+  var output = false
+
+  assert.deepEqual(E.isLastInStock(input), output)
+
+  assert.end()
+})
